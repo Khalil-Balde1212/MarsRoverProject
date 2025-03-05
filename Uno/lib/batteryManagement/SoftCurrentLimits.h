1d1976f994f37bf9e9f54441d988c8bac0804123
Khalil-Balde1212 <khalil12345385@gmail.com>
2025-03-05 11:08:38 -0500

    Init stuff

---
+++ Uno/lib/batteryManagement/SoftCurrentLimits.h
@@ -0,0 +1,12 @@
+#ifndef SOFT_CURRENT_LIMITS_H
+#define SOFT_CURRENT_LIMITS_H
+
+#include "../robot_const.h"
+#include <Arduino.h>
+
+namespace pms
+{
+    
+}
+
+#endif
---
+++ Uno/lib/robot_const.h
@@ -0,0 +1,6 @@
+#ifndef ROBOT_CONST_H
+#define ROBOT_CONST_H
+
+
+
+#endif
