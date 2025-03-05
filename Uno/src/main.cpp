eee79bc5f5db65a6c90a371b7abac7af434a8aec
Khalil-Balde1212 <khalil12345385@gmail.com>
2025-03-05 11:18:24 -0500

    created motor function

--- Uno/src/main.cpp
+++ Uno/src/main.cpp
@@ -1,20 +1,37 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define MOTOR_PIN1 A0
+#define MOTOR_PIN2 A1
+#define MOTOR_PIN3 A2
+#define MOTOR_PIN4 A4
+
+/**
+ * Some speed between -1 and 1
+ */
+void runMotor(double speed){
+  //clamp input
+  if (speed > 1) {
+    speed = 1;
+  } else if (speed < -1) {
+    speed = -1;
+  }
+  
+  if(speed > 0){
+    analogWrite(MOTOR_PIN1, 255 * speed);
+    analogWrite(MOTOR_PIN2, 0);
+  } else {
+    analogWrite(MOTOR_PIN1, 0);
+    analogWrite(MOTOR_PIN2, 255 * -speed);
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(5, 10);
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println("YOHOHOHO");
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x * y;
+  runMotor(0.5);
+  delay(1000);
+  runMotor(-0.5);
+  delay(1000);
 }
