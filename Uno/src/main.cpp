b501685290036447f191732dd25f0322d26879c3
Khalil-Balde1212 <khalil12345385@gmail.com>
2025-03-06 10:06:09 -0500

    Flip floppin motor

--- Uno/src/main.cpp
+++ Uno/src/main.cpp
@@ -1,24 +1,35 @@
 #include <Arduino.h>
 #include <Adafruit_PWMServoDriver.h>
 
+#include "robot_const.h"
+
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 
-// put function declarations here:
-int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(5, 10);
   Serial.begin(9600);
+  pwm.begin();
+  pwm.setPWMFreq(800); //TODO check for better frequencies
+  Serial.println("init PWM Servo Driver");
+
+
+  Wire.setClock(400000); // 400kHz I2C clock
+  Wire.begin();
+
+
 }
 
 void loop() {
-  Serial.println("YOHOHOHO");
-  // put your main code here, to run repeatedly:
-}
+  Serial.println("forward");
+  pwm.setPWM(0, 0, 4095);
+  pwm.setPWM(FL_MOTORA, 0, 4095);
+  pwm.setPWM(FL_MOTORB, 0, 0);
+  delay(1000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x * y;
+  Serial.println("backward");
+  pwm.setPWM(0, 0, 0);
+  pwm.setPWM(FL_MOTORA, 0, 0);
+  pwm.setPWM(FL_MOTORB, 0, 4095);
+  delay(1000);
 }
---
+++ Uno/src/robot_const.h
@@ -0,0 +1,24 @@
+#ifndef ROBOT_CONST_H
+#define ROBOT_CONST_H
+
+
+//motor definitions
+#define FL_MOTORA 5
+#define FL_MOTORB 6
+
+//TODO define the rest
+#define CL_MOTORA -1
+#define CL_MOTORB -1
+#define BL_MOTORA -1
+#define BL_MOTORB -1
+
+#define FR_MOTORA -1
+#define FR_MOTORB -1
+#define CR_MOTORA -1
+#define CR_MOTORB -1
+#define BR_MOTORA -1
+#define BR_MOTORB -1
+
+
+
+#endif
