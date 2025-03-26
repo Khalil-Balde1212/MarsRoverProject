808827166f2816db649af54df2588138d8388a95
drakflygare <sungamt@gmail.com>
2025-03-25 22:24:01 -0400

    all sensor code, old code gone

--- Uno/src/main.cpp
+++ Uno/src/main.cpp
@@ -1,10 +1,8 @@
-#include <Arduino.h>
 #include <Adafruit_PWMServoDriver.h>
-#include <psdISR.h>
-
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #include "robot_const.h"
 
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
+#include <Servo.h>
 
 #define SERVOMIN  65 // This is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  465 // This is the 'maximum' pulse length count (out of 4096)
@@ -19,77 +17,88 @@ const byte interruptPin = 3;  //PSD interrupt pin
 volatile byte state = HIGH; //sets interrupt to HIGH intially
 //current
 const byte currentPin = A1; //current sensor input pin
-const float Vreference = 5; //arduino internal reference voltage
+const float Vreference = 5.0; //arduino internal reference voltage
+const float volts_per_amp = 0.2f;
 const float offsetV = 0;  //offset voltage
-const float sensitivity = 0;  //current sensor sensitivity
+const float sensitivity = 0.185;  //current sensor sensitivity
 float currentSensorValue = 0; //reads the value from the current sensor
 //photo
-const byte photoPin = A2; //photo sensor input pin
+const byte photoPin = A0; //photo sensor input pin
 float photoValue = 0; //to read photo value
 
+Servo Servo1; 
 
 void setup() {
+  Serial.println("setup");
   Serial.begin(9600);
+  pinMode(interruptPin, INPUT);
+  pinMode(currentPin, INPUT);
+  pinMode(photoPin, INPUT);
   pwm.begin();
   pwm.setPWMFreq(SERVO_FREQ); //TODO check for better frequencies
   //believe 50 is the standard
   pwm.setOscillatorFrequency(27000000); //shouldn't change anything as 27Mhz should be the original setting
+  Servo1.attach(10); 
   Serial.println("init PWM Servo Driver");
-
-  pinMode(interruptPin, INPUT);
-  pinMode(currentPin, INPUT);
-  pinMode(photoPin, INPUT);
+  
   //interrupt for PSD distance sensor at 10cm using 9.1k and 1k resistors for R2 and R1 respectively
   attachInterrupt(digitalPinToInterrupt(interruptPin), psdInterrupt, FALLING);
 
 
   Wire.setClock(400000); // 400kHz I2C clock
   Wire.begin();
-
 }
 
-void loop() {
-  //turn off motors when it is dark
-  while(photoSens != 0) { //replace with appropriate while condition
-    //set motors to zero
-  }
+void psdInterrupt() {
+  //set all motors to 0
+  Serial.println("interrupted");
+  volatile byte state = HIGH;
+}
 
-  //current checker, where is it going?
+void printCurrent(){
+  Serial.print("Current: ");
+  Serial.print(readCurrent());
+  Serial.println(" A");
+}
+
+float readCurrent(){
   currentSensorValue = analogRead(currentPin);
-  float currentVoltage = currentSensorValue * (Vreference / 1023.0);
-  float current = (currentVoltage - offsetV) / sensitivity;
-  if(current <= 0.2) {
+  float voltage = currentSensorValue * (Vreference/1023.0); // Convert analog value to voltage
+  voltage -= 2.5;
+  float current = voltage/volts_per_amp;
+  return current;
+}
+void loop() {
+  //Serial.println("running");
+  delay(150);
+  printCurrent();
 
-    //do something and set proper if statement value
+  Servo1.write(0); 
+  delay(1000); 
+  // Make servo go to 90 degrees 
+  Servo1.write(90); 
+  delay(1000); 
+  // Make servo go to 180 degrees 
+  Servo1.write(180); 
+  delay(1000);
 
+  /*
+  Serial.println("why");
+  pwm.setPWM(10, 0, 4095);
+  delay(1000);
+  //pwm.writeMicroseconds(servonum, 2500);
+  pwm.setPWM(10, 4095, 0);
+  delay(1000);
+  */
+  
+  int  value = analogRead(photoPin);
+  if (value > 30)
+  {
+    Serial.println("high");
+    //run rover as normal
   }
-
-  //photo sensor to turn off when dark
-  photoValue = analogRead(photoPin);
-  if(photoValue < 250) {  //whatever value we want
-    //set all motors to 0
-    for(int i = 0; i < 6; i++) {
-      pwm.writeMicroseconds(i, 500);
-    }
+  else{
+    Serial.println("low");
+    //stop rover motion
   }
-
-  //code to just turn the servo 
-  Serial.println(servonum);
-  pwm.writeMicroseconds(servonum, 500);
-  delay(1000);
-  pwm.writeMicroseconds(servonum, 2500);
-  delay(1000);
-  //delete or replace when more servo controls are added
-
-  Serial.println("forward");
-  pwm.setPWM(0, 0, 4095);
-  pwm.setPWM(FL_MOTORA, 0, 4095);
-  pwm.setPWM(FL_MOTORB, 0, 0);
-  delay(1000);
-
-  Serial.println("backward");
-  pwm.setPWM(0, 0, 0);
-  pwm.setPWM(FL_MOTORA, 0, 0);
-  pwm.setPWM(FL_MOTORB, 0, 4095);
-  delay(1000);
 }
